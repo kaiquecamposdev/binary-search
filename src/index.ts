8717748618dd29b81d62f1c4c861a9595de8ae45
kaiquecamposdev <kaiquedecampos2@gmail.com>
2024-12-20 19:00:32 -0300

    feat: update index.ts to include exercise 5 and log output for exercise 4

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { exercise1 } from './exercises/1.1-exercise.ts';
 import { exercise2 } from './exercises/1.2-exercise.ts';
 import { exercise3 } from './exercises/1.3-exercise.ts';
-import {
-  exercise4
-} from './exercises/1.4-exercise.ts';
+import { exercise4 } from './exercises/1.4-exercise.ts';
+import { exercise5 } from './exercises/1.5-exercise.ts';
 
 exercise1()
 console.log('-----------------')
@@ -12,3 +11,5 @@ console.log('-----------------')
 exercise3()
 console.log('-----------------')
 exercise4()
+console.log('-----------------')
+exercise5()
