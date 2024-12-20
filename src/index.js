fcd3e49b621780ca4e8e26905c7bef65f0be1c38
kaiquecamposdev <kaiquedecampos2@gmail.com>
2024-12-20 16:52:20 -0300

    feat: migrate index.js to TypeScript and include exercise4

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { exercise1 } from './exercises/1.1-exercise.js';
-import { exercise2 } from './exercises/1.2-exercise.js';
-import { exercise3 } from './exercises/1.3-exercise.js';
-
-exercise1()
-console.log('-----------------')
-exercise2()
-console.log('-----------------')
-exercise3()
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { exercise1 } from './exercises/1.1-exercise.ts';
+import { exercise2 } from './exercises/1.2-exercise.ts';
+import { exercise3 } from './exercises/1.3-exercise.ts';
+import {
+  exercise4
+} from './exercises/1.4-exercise.ts';
+
+exercise1()
+console.log('-----------------')
+exercise2()
+console.log('-----------------')
+exercise3()
+console.log('-----------------')
+exercise4()
