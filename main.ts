8f8a9e34f3e7c89abf8de77dcf3ce84400549a0a
kaiquecamposdev <kaiquedecampos2@gmail.com>
2024-12-23 11:33:32 -0300

    feat: integrate countdown, greet, and factorial functions into main execution flow

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 import { binarySearchEx } from "./01-introduction-to-algorithms/01-binary-search.ts"
 import { selectionSortEx } from "./02-selection-sort/01-selection-sort.ts"
+import { countdownEx } from "./03-recursion/01-countdown.ts"
+import { greetEx } from "./03-recursion/02-greet.ts"
+import { factorialEx } from "./03-recursion/03-factorial.ts"
 
 
 binarySearchEx()
 console.log('-----------------')
 selectionSortEx()
+console.log('-----------------')
+countdownEx()
+console.log('-----------------')
+greetEx()
+console.log('-----------------')
+factorialEx()
 console.log('-----------------')
