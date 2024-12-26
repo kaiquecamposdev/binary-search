f59006dd4ab17688e95a85b6a7715a972ae51dad
kaiquecamposdev <kaiquedecampos2@gmail.com>
2024-12-26 11:24:57 -0300

    refactor: update binary search function parameters and improve variable naming

--- 01-introduction-to-algorithms/01-binary-search.ts
+++ 01-introduction-to-algorithms/01-binary-search.ts
@@ -1,43 +1,43 @@
-import { measureExecutionTime } from '../utils/measure-execution-time.ts'
 
 
 
-const binarySearch = (items: number[], item: number) => {
+const binarySearch = (item: number, items: number[]) => {
   let low = 0
   let high = items.length - 1
   let middle = 0
-  let tentative = 0
+  let attempt = 0
+  // base case
   let results = []
 
   while (low <= high) {
-    middle = Math.ceil((low + high) / 2) 
-    tentative = items[middle]
-    
-    if (tentative === item) { 
-      return middle
-    } else if (tentative > item) { 
+    middle = Math.ceil((low + high) / 2)
+    attempt = items[middle]
+
+    if (attempt === item) { 
+      return results.length
+    }
+    if (attempt > item) { 
       high = middle - 1
-    } else { 
+    } 
+    if (attempt < item) { 
       low = middle + 1
     }
 
-    results.push(tentative)
+    results.push(attempt)
   }
-  
+
   return results.length
 }
 
 export const binarySearchEx = () => {
   let numbers = []
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i <= 100; i++) {
     numbers.push(i)
   }
 
-  const execute = measureExecutionTime(binarySearch) 
-
   console.log(`
     Exercise 1.1 -> Binary search\r
-    Max iterations: ${execute(numbers, 9)}
+    Max iterations: ${binarySearch(53, numbers)}
   `)
 }
