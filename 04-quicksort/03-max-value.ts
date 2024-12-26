3ba137f8ee6c42fc2a4380ef3ce3f4a9d70942ea
kaiquecamposdev <kaiquedecampos2@gmail.com>
2024-12-26 11:25:37 -0300

    feat: add maxValue function to find the maximum number in an array

---
+++ 04-quicksort/03-max-value.ts
@@ -0,0 +1,11 @@
+const maxValue = (numbers: number[]) => {
+  const max = Math.max(...numbers)
+
+  return max
+}
+
+export const maxValueEx = () => {
+  const numbers = [0, 3, 2, 1, 23, 9, 42, 44, 12, 5, 88, 24]
+
+  console.log(maxValue(numbers))
+}
