e70f6e5fda08274b34908d82070716fe5746ec7d
kaiquecamposdev <kaiquedecampos2@gmail.com>
2024-12-26 11:25:18 -0300

    feat: add sum function to calculate total of an array of numbers

---
+++ 04-quicksort/01-sum.ts
@@ -0,0 +1,15 @@
+const sum = (numbers: number[]): number => {
+  let total = 0
+
+  for (let x of numbers) {
+    total += x
+  }
+
+  return total
+}
+
+export const sumEx = () => {
+  const numbers = [1, 9, 10, 32, 54, 22]
+
+  console.log(sum(numbers))
+}
